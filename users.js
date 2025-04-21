c72827b9259fcaaa23a8cfda60e54c352402fa76
AnnaGary <97687775+AnnaGary@users.noreply.github.com>
2025-04-20 22:28:24 -0500

    8

--- users.js
+++ users.js
@@ -8,11 +8,10 @@ const userSchema = new Schema({
     bio: { type: String, maxlength: 100 },        
     hobbies: { type: [String], required: true },  
     location: {type: String},              
-    matches: [ 
-        null
-            //{userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-            //status: String}
-            ],
+    matches: [{
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        status: String
+    }],
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }],
     createdAt: { type: Date, default: Date.now },
     role: { type: String, enum: ["user", "admin"], default: "user" },
